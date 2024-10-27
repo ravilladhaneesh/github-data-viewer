0d1663611ecc513cd1222b541a23f499826ad2bd
ravilladhaneesh <ravilladhaneesh@gmail.com>
2024-10-27 19:30:55 +0530

    change s3 bucket region

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "tf-state-bucket-github-viewer"
     key    = "tf-files/state/terraform.tfstate"
-    region = "us-west-2"
+    region = "ap-south-1"
   }
 }
